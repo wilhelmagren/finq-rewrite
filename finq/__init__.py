abc5c158546c35a5b4b0b8a31e0470287c2660bf
wilhelmagren <wilhelmagren98@gmail.com>
2024-05-17 00:22:32 +0200

    [fix] previous year in license

--- finq/__init__.py
+++ finq/__init__.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2023 Wilhelm Ågren
+Copyright (c) 2024 Wilhelm Ågren
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
